77744db490b78421c795badb9538a73f2706ab2a
Accacio <accaciolucas@gmail.com>
2022-12-14 05:46:20 -0300

    Add files via upload

---
+++ src/main/java/controllers/PrimeiroController.java
@@ -0,0 +1,15 @@
+package controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PrimeiroController {
+
+	@GetMapping("/")
+	public String ola() {
+		return "Spring funcionando";
+	}
+}
