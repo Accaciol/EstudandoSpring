e43e8cd6f107090c8c47b4427880d41e5cfdbed0
Bonfim Macedo <caio.bonfimmacedo@dxc.com>
2022-12-14 08:56:15 -0300

    Movendo pacote Controllers para o locl certo

---
+++ src/main/java/br/com/cod3r/exerciciossb/controllers/PrimeiroController.java
@@ -0,0 +1,13 @@
+package br.com.cod3r.exerciciossb.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PrimeiroController {
+
+	@GetMapping("/")
+	public String ola() {
+		return "Spring funcionando";
+	}
+}
