eb92380e7d9ca4583ab4f49e3c78079f85cede1e
Lucas <Lucas@Accacio.mshome.net>
2022-12-18 16:35:55 -0300

    criando Cliente

---
+++ src/main/java/br/com/cod3r/exerciciossb/controllers/ClienteController.java
@@ -0,0 +1,17 @@
+package br.com.cod3r.exerciciossb.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.cod3r.exerciciossb.models.Cliente;
+
+@RestController
+@RequestMapping(path = "/clientes")
+public class ClienteController {
+
+	@GetMapping(path = "/qualquer")
+	public Cliente obterCliente() {
+		return new Cliente(28, "pedro", "123.456.789-000");
+	}
+}
--- src/main/java/br/com/cod3r/exerciciossb/controllers/PrimeiroController.java
+++ src/main/java/br/com/cod3r/exerciciossb/controllers/PrimeiroController.java
@@ -6,8 +6,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PrimeiroController {
 
-	@GetMapping("/")
-	public String ola() {
+	
+	@GetMapping(path = "/")
+	public String index() {
 		return "Spring funcionando";
+	}
+	
+	@GetMapping(path = "/ola")
+	public String ola() {
+		return "Spring get";
+	}
+	
+	
+	
+	@GetMapping(path = "/saudacao")
+	public String saudacao() {
+		return "Spring post";
 	}
 }
